f9e5a34bf454049b63c7896c2e767a4129af139a
jsnetors <jsnetors@gmail.com>
2020-03-12 16:16:45 +0000

    Atualização do arquivo de referência

--- 00_TRefs.c
+++ 00_TRefs.c
@@ -1,10 +1,10 @@
 Este arquivo agrega comandos e instruções do terminal Linux para referência.
 
-Criar um novo arquivo
+//Criar um novo arquivo
 cat > nomedoarquivo
 
 //Protocolo de commit do Git
-cd dev
-git add .
-git commit -m "descrição das ações tomadas"
-git push origin master
+cd dev //Ou ir para a pasta a ser commited
+git add . //Adiciona todos os arquivos da pasta
+git commit -m "descrição das ações tomadas" //Auto-explicativo
+git push origin master //Manda pro servidor
