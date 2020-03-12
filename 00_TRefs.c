27e0eda4788782ba0e44b024cc1524ca835de674
jsnetors <jsnetors@gmail.com>
2020-03-12 16:13:09 +0000

    added reference file

---
+++ 00_TRefs.c
@@ -0,0 +1,10 @@
+Este arquivo agrega comandos e instruções do terminal Linux para referência.
+
+Criar um novo arquivo
+cat > nomedoarquivo
+
+//Protocolo de commit do Git
+cd dev
+git add .
+git commit -m "descrição das ações tomadas"
+git push origin master
