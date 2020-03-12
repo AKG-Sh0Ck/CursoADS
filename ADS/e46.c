1c4f5686c2da6b8ff6f516297243a55501372da6
jsnetors <jsnetors@gmail.com>
2020-03-12 05:35:33 +0000

    Exercício e46 concluído

--- ADS/e46.c
+++ ADS/e46.c
@@ -1,22 +1,56 @@
+//Nesse código, considerei a data mais recente como a maior.
 #include <stdio.h>
 
 int main(void)
 {
-    double data1;
-    double data2;
+    int D1, M1, A1;
+    int D2, M2, A2;
 
-    printf("Digite uma data (DDMMAAAA)...\n");
-    scanf("%lf", &data1);
-    printf("Digite outra data (DDMMAAAA)...\n");
-    scanf("%lf", &data2);
+    printf("Digite uma data (DD/MM/AAAA)...\n");
+    scanf("%i%*C%i%*C%i", &D1, &M1, &A1); //Busca os três conjuntos de números ignorando os caracteres não-numéricos
     
-    if (data1 > data2)
+    printf("Digite outra data (DD/MM/AAAA)...\n");
+    scanf("%i%*C%i%*C%i", &D2, &M2, &A2); //Busca os três conjuntos de números ignorando os caracteres não-numéricos
+    
+//Verifica se as datas são iguais
+    if (A1 == A2 && M1 == M2 && D1 == D2)
     {
-        printf("%.0f é a data maior.\n", data1);
+        printf("As datas são iguais!\n");
+    }
+
+//Verifica qual ano é maior
+    if (A1 > A2)
+    {
+        printf("%i/%i/%i é a data maior.\n", D1, M1, A1);    
     }
     else 
     {
-        printf("%.0f é a data maior.\n", data2);
+        printf("%i/%i/%i é a data maior.\n", D2, M2, A2);
+    }    
+
+//Caso os anos sejam iguais, compara os meses
+    if (A1 == A2)
+    {
+            if (M1 > M2)
+        {
+            printf("%i/%i/%i é a data maior.\n", D1, M1, A1);    
+        }
+        else 
+        {
+            printf("%i/%i/%i é a data maior.\n", D2, M2, A2);
+        }       
+    }    
+
+//Caso os meses também sejam iguais, compara os dias
+    if (M1 == M2)
+    {
+            if (D1 > D2)
+        {
+            printf("%i/%i/%i é a data maior.\n", D1, M1, A1);    
+        }
+        else 
+        {
+            printf("%i/%i/%i é a data maior.\n", D2, M2, A2);
+        }       
     }
-    return 0;
 }
