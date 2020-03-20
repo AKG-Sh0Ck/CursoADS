62576bf6c1bdf224d4b13e021e42d655b284f6e2
jsnetors <jsnetors@gmail.com>
2020-03-20 19:32:21 +0000

    e57 pronto

---
+++ ADS/e57.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+int x, y;
+char quad;
+
+int main(void)
+{
+
+    printf("Digite a coordenada X:\n");
+    scanf("%i", &x);
+
+    printf("Digite a coordenada Y:\n");
+    scanf("%i", &y);
+        
+    if (x > 0 && y > 0)
+    {
+        quad = 'A';
+    }
+        
+    else if (x > 0 && y < 0)
+    {
+        quad = 'B';
+    }
+        
+    else if (x < 0 && y < 0)
+    {
+        quad = 'C';
+    }
+        
+    else if (x < 0 && y > 0)
+    {
+        quad = 'D';
+    }
+    
+    else 
+    {
+        if (x == 0)
+            {
+                printf("O ponto está sobre o eixo cartesiano X.\n");
+            }       
+        if (y == 0)
+            {
+                printf("O ponto está sobre o eixo cartesiano Y.\n");
+            }
+        return 0;
+    }
+    
+    printf("O ponto está no quadrante %c\n", quad);
+    return 0;
+}
